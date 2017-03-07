1099a7484dcef26004d1ac14d2438e544745fe4b
Joy Earl <jearl@newrelic.com>
2017-03-06 22:45:09 -0800

    adding fetch()

--- src/UserInput.js
+++ src/UserInput.js
@@ -4,11 +4,14 @@ import React, { Component } from 'react';
 export default class UserInput extends Component {
   constructor(props) {
     super(props)
-    this.state = {value: ''};
+    this.state = {
+      value: '',
+      data: null};
 
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+
   }
 
     handleSubmit(event) {
@@ -20,8 +23,21 @@ export default class UserInput extends Component {
       this.setState({value: event.target.value});
   }
 
+  componentDidMount() {
+    let URL = 'http://api.openweathermap.org/data/2.5/weather?zip=97231,us&APPID=313186e768d1b0e1e4e192f966703b6e'
+    fetch(URL)
+    .then((response) => response.json())
+    .then((json) => this.setState({data: json.weather.description})
+
+  )}
+
+//http://stackoverflow.com/questions/39436025/accessing-object-attributes-in-the-react-render-method/39436150///
+
+
+
     render() {
-      return (
+      return(
+      <div>
         <form className="input" onSubmit={this.handleSubmit}>
           <label>
             Enter Your Zipcode
@@ -29,9 +45,11 @@ export default class UserInput extends Component {
             <input type="text" value={this.state.value} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Submit" />
+
+            <p>{this.state.data}</p>
         </form>
-
+        </div>
       );
-    }
 
+    }
 }
