bb0ad30664db7439a8b702d7a97edb8626d4bec5
Joy Earl <jearl@newrelic.com>
2017-03-07 23:02:54 -0800

    more work on api

--- src/UserInput.js
+++ src/UserInput.js
@@ -6,11 +6,14 @@ export default class UserInput extends Component {
     super(props)
     this.state = {
       value: '',
-      data: null};
-
+      forecast: {
+        forecast: []
+      },
+    }
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.setWeather = this.setWeather.bind(this);
 
   }
 
@@ -23,21 +26,46 @@ export default class UserInput extends Component {
       this.setState({value: event.target.value});
   }
 
-  componentDidMount() {
+  getWeather () {
+    let self = this;
     let URL = 'http://api.openweathermap.org/data/2.5/weather?zip=97231,us&APPID=313186e768d1b0e1e4e192f966703b6e'
     fetch(URL)
-    .then((response) => response.json())
-    .then((json) => this.setState({data: json.weather.description})
+    .then(function (response) {
+      if (response.status !== 200) {
+      console.log('oopsies. Status Code: ' + response.status);
+      return;
+    }
+    response.json().then(function(data) {
+      self.setWeather(data);
+    });
+  })
+  .catch (function (error) {
+    console.log('Get Error : -S', error);
+  });
+}
 
-  )}
+  setWeather(forecast) {
+    this.setState({
+      forecast: forecast
+    });
+  }
+
+  componentWillMount() {
+    this.getWeather();
+  }
 
 //http://stackoverflow.com/questions/39436025/accessing-object-attributes-in-the-react-render-method/39436150///
 
 
 
     render() {
-      return(
+      const { forecast } = this.state;
+      return (
+
       <div>
+        <div>
+        <h1>{forecast.name}</h1>
+      </div>
         <form className="input" onSubmit={this.handleSubmit}>
           <label>
             Enter Your Zipcode
@@ -45,8 +73,6 @@ export default class UserInput extends Component {
             <input type="text" value={this.state.value} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Submit" />
-
-            <p>{this.state.data}</p>
         </form>
         </div>
       );
