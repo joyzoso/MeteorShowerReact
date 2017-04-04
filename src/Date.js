deeb098eb3dd068d6ddb1fa2bf209ef3967c8818
Joy Earl <joyrockon@gmail.com>
2017-04-04 12:57:56 -0700

    displayed event based on current month

--- src/Date.js
+++ src/Date.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 import Moment from 'moment';
- 
+import { showers } from './Constants';
+
 export default class Date extends Component {
- 
+
+
+  determineShower(sampleShower, currentMonth) {
+      if (currentMonth === sampleShower[0].startMonth || currentMonth === sampleShower[0].endMonth) {
+        return sampleShower[0].event;
+      }
+
+    }
+
 
     render() {
         let date = Moment();
         let formattedDate = Moment(date).format('MMMM Do YYYY, h:mm a')
-         
+
+        // showers - compare the current month with the list of showers
+        let sampleShower = [{date: "April",
+                            event: "Lyrids",
+                            peak: "4/11 - 4/22",
+                            startMonth: 3,
+                            endMonth: 3}
+                      ];
+
+        let currentMonth = date.month();
+
+
+        // let showerMonth = Moment().set('month', 'April')
+        console.log(currentMonth);
+
+
+
+
              return(
                 // is is the local current time? need more moment mumbo jumbo for that
                <div className="date">
                   <p>The local current time is {formattedDate}</p>
-                  <p> The next upcoming event will be ... </p>
+                  <p> The next upcoming event will be ...{this.determineShower(sampleShower, currentMonth)} </p>
                </div>
                  )
     }
+
+
 }
 
 // so this is where I would want to have the date object pull the corresponding month and display the next event(s)
-// but along with that I also want a modal to have separate information, so should I have the Modal (or whatever) 
+// but along with that I also want a modal to have separate information, so should I have the Modal (or whatever)
 // component deal with that logic or have it here in the date component? hmmmm
-
-
-
-
--- src/Meteors.js
+++ src/Meteors.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { showers } from './Constants';
 
 
 export default class Meteors extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      showers:[
-          {date: "April", event: "Lyrids", peak: "4/11 - 4/22"},
-          {date: "May", event: "Eta Aquarids", peak: "4/19 - 4/26"},
-          {date: "July ", event: "Alpha Capricornids", peak: "7/26 - 7/27"},
-          {date: "August", event: "Perseids", peak: "8/11 - 8/12"},
-          {date: "October", event: "Southern Taurids", peak: "10/9 - 10/10"},
-          {date: "October", event: "Orionids", peak: "10/21 - 10/22"},
-          {date: "November", event: "Northern Taurids", peak: "11/10 - 11/11"},
-          {date: "November", event: "Leonids", peak: "11/17 - 11/18"},
-          {date: "December", event: "Geminids", peak: "12/13 - 12/14"},
-          {date: "December", event: "Ursids", peak: "12/21 - 12/22"},
-
-          ]
-      }
+    this.state = showers;
 
       this.showShowers = this.showShowers.bind(this);
 
@@ -62,4 +48,3 @@ export default class Meteors extends Component {
     )
   }
 }
-
